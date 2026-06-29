5f371b861617e0470bbae6c8816651eb0e9fe125
Vanbunchhai-ops <vanbunchhai@gmail.com>
2026-06-29 23:42:11 +0700

    Frontend 3.0 vue router

--- vuejs-app/src/router/index.js
+++ vuejs-app/src/router/index.js
@@ -1,14 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import SigninView from '../views/SigninView.vue'
-import SignupView from '../views/SignupView.vue'
+import Signin from '@/components/auth/Signin.vue';
+import Signout from '@/components/auth/Signout.vue';
+import Signup from '@/components/auth/Signup.vue';
+import Dashboard from '@/components/pages/Dashboard.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/signin', name: 'signin', component: SigninView },
-    { path: '/signup', name: 'signup', component: SignupView },
+    {
+      path: '/',
+      name: 'auth.signin',
+      component: Signin,
+    },
+    {
+      path: '/signout',
+      name: 'auth.signout',
+      component: Signout,
+    },
+    {
+      path: '/signup',
+      name: 'auth.signup',
+      component: Signup,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/dashboard',
+    }
   ],
 })
 
